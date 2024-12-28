1078d8cb24e5b6f6e4ce8032e75a43540beabb61
Rija-Shah98 <zurr.ch65@gmail.com>
2024-12-28 11:36:30 +0500

    Fixed constructor issues and improved RedisServer code

--- server.h
+++ server.h
@@ -1,35 +1,35 @@
+#ifndef SERVER_H
+#define SERVER_H
+
 #include <boost/asio.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <unordered_map>
-#include <string>
 #include <memory>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
-#include "command.h"
-#include "utils.h"
+class Connection : public std::enable_shared_from_this<Connection> {
+public:
+    explicit Connection(boost::asio::io_context& io_context)
+        : socket(io_context) {
+    }
+
+    boost::asio::ip::tcp::socket socket;
+    std::string command;
+    std::vector<char> wbuf;
+};
 
 class RedisServer {
 public:
     RedisServer(boost::asio::io_context& io_context, int port);
-    void runServer();
-
-private:
-    struct Connection {
-        boost::asio::ip::tcp::socket socket;
-        std::vector<char> rbuf; // Read buffer
-        std::vector<char> wbuf; // Write buffer
-        std::string command;
-
-        explicit Connection(boost::asio::io_context& io_context)
-            : socket(io_context), rbuf(1024), wbuf(1024) {
-        }
-    };
-
     void acceptConnection();
     void handleRead(std::shared_ptr<Connection> conn);
     void handleWrite(std::shared_ptr<Connection> conn, const std::string& response);
+    std::string processCommand(const std::string& command);
 
-    std::unordered_map<std::string, std::string> kvstore;
+private:
     boost::asio::ip::tcp::acceptor acceptor_;
+    std::unordered_map<std::string, std::string> kvstore;
 };
+
+#endif
+
